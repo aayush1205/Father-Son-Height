fe003189b210aad1e9b5ae0bc69e989267b7d0a0
Aayush Upadhyay <bfmv11121@gmail.com>
2018-09-28 19:44:55 +0530

    Update predict_height.m

--- predict_height.m
+++ predict_height.m
@@ -3,3 +3,6 @@ data = load('Pearson.txt');
 %Declaring Father's and Son's height in variables
 X = data(:,1) ; y = data(:,2);
 m=length(y);
+%Adding bias to Father's Height
+X = [ones(m,1) , data(:,1)];
+
