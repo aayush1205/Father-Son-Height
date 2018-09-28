f2b65ec81a26b10bec976fad1d36f31d810c7b49
Aayush Upadhyay <bfmv11121@gmail.com>
2018-09-28 19:47:45 +0530

    Update predict_height.m

--- predict_height.m
+++ predict_height.m
@@ -1,10 +1,19 @@
 %Importing the data set available
 data = load('Pearson.txt');
+
 %Declaring Father's and Son's height in variables
 X = data(:,1) ; y = data(:,2);
 m=length(y);
+
 %Adding bias to Father's Height
 X = [ones(m,1) , data(:,1)];
+
 %generating theta
 theta = pinv(X'*X)*X'*y;
+
+%Plotting the data set with the generated hypothesis
+
+plot(data(:,1) , y , 'ro' , 'MarkerSize' , 10);
+hold on
+plot(data(:,1) , X*theta , '-');
 
